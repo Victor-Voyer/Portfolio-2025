10c041723c6091276ba1839ff2a982c744a74f62
Victor-Voyer <victor.voyer@oclock.school>
2025-10-20 15:42:56 +0200

    Configuration pour GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio-2025/', // Remplacez par le nom de votre repository
   server: {
     port: 3000,
     open: true
